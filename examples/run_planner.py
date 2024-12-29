9c6e4f419fd22e4c8b9ffa07a32bcc821eab6067
VH <v@vhartmann.com>
2024-12-29 11:28:34 +0100

    Add simple shortcuttig

--- examples/run_planner.py
+++ examples/run_planner.py
@@ -23,6 +23,96 @@ from multi_robot_multi_goal_planning.planners.tensor_prm_planner import (
 
 # np.random.seed(100)
 
+
+def robot_mode_shortcut(env: rai_env, path: List[State], max_iter: int = 1000):
+    new_path = interpolate_path(path, 0.05)
+
+    costs = [path_cost(new_path, env.batch_config_cost)]
+    times = [0.0]
+
+    start_time = time.time()
+
+    config_type = type(env.get_start_pos())
+
+    cnt = 0
+    for iter in range(max_iter):
+        i = np.random.randint(0, len(new_path))
+        j = np.random.randint(0, len(new_path))
+
+        if i > j:
+            tmp = i
+            i = j
+            j = tmp
+
+        if abs(j - i) < 2:
+            continue
+
+        robots_to_shortcut = [r for r in range(len(env.robots))]
+        random.shuffle(robots_to_shortcut)
+        # num_robots = np.random.randint(0, len(robots_to_shortcut))
+        num_robots = 1
+        robots_to_shortcut = robots_to_shortcut[:num_robots]
+
+        can_shortcut_this = True
+        for r in robots_to_shortcut:
+            if new_path[i].mode[r] != new_path[j].mode[r]:
+                can_shortcut_this = False
+
+        if not can_shortcut_this:
+            continue
+
+        cnt += 1
+
+        q0 = new_path[i].q
+        q1 = new_path[j].q
+
+        # constuct pth element for the shortcut
+        path_element = []
+        for k in range(j - i + 1):
+            q = []
+            for r in range(len(env.robots)):
+                # print(r, i, j, k)
+                if r in robots_to_shortcut:
+                    q_interp = q0[r] + (q1[r] - q0[r]) / (j - i) * k
+                    q.append(q_interp)
+                else:
+                    q.append(new_path[i + k].q[r])
+
+            # print(q)
+            path_element.append(State(config_type.from_list(q), new_path[i + k].mode))
+
+        # check if the shortcut improves cost
+        if path_cost(path_element, env.batch_config_cost) >= path_cost(
+            new_path[i : j + 1], env.batch_config_cost
+        ):
+            continue
+
+        # this is wrong for partial shortcuts atm.
+        if env.is_path_collision_free(path_element):
+            for k in range(j - i + 1):
+                new_path[i + k].q = path_element[k].q
+
+                # if not np.array_equal(new_path[i+k].mode, path_element[k].mode):
+                # print('fucked up')
+
+        current_time = time.time()
+        times.append(current_time - start_time)
+        costs.append(path_cost(new_path, env.batch_config_cost))
+
+    assert new_path[-1].mode == path[-1].mode
+    assert np.linalg.norm(new_path[-1].q.state() - path[-1].q.state()) < 1e-6
+    assert np.linalg.norm(new_path[0].q.state() - path[0].q.state()) < 1e-6
+
+    print("original cost:", path_cost(path, env.batch_config_cost))
+    print("Attempted shortcuts", cnt)
+    print("new cost:", path_cost(new_path, env.batch_config_cost))
+
+    # plt.plot(times, costs)
+    # plt.show()
+
+    return new_path, [costs, times]
+
+
 def interpolate_path(path: List[State], resolution: float = 0.1):
     config_type = type(path[0].q)
     new_path = []
@@ -84,10 +174,13 @@ def main():
     )
     interpolated_path = interpolate_path(path, 0.05)
 
+    shortcut_path, info_shortcut = robot_mode_shortcut(env, path, 10000)
 
     print("Checking original path for validity")
     print(env.is_valid_plan(interpolated_path))
 
+    print("Checking shortcutted path for validity")
+    print(env.is_valid_plan(shortcut_path))
 
     print("cost", info["costs"])
     print("comp_time", info["times"])
@@ -95,11 +188,21 @@ def main():
     plt.figure()
     plt.plot(info["times"], info["costs"], "o")
 
+    plt.figure()
+    for info in [info_shortcut]:
+        plt.plot(info[1], info[0])
+
+    plt.xlabel("time")
+    plt.ylabel("cost")
+
     plt.show()
 
     print("displaying path from prioritized planner")
     display_path(env, interpolated_path, stop=False)
 
+    print("displaying path from shortcut path")
+    shortcut_discretized_path = interpolate_path(shortcut_path)
+    display_path(env, shortcut_discretized_path, stop=False)
 
 
 if __name__ == "__main__":
