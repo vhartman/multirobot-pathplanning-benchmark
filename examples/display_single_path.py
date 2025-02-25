42a4729ad52f3c9d94c8fc67b234f0620af98e48
Tirza <theinle@ethz.ch>
2025-02-25 15:59:03 +0100

    Change: Set correct random env seed for display

--- examples/display_single_path.py
+++ examples/display_single_path.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import datetime
 import json
 import os
+import re
 import random
 
 import numpy as np
@@ -19,7 +20,7 @@ from multi_robot_multi_goal_planning.planners.shortcutting import (
     single_mode_shortcut,
     robot_mode_shortcut,
 )
-
+from run_experiment import load_experiment_config
 # from multi_robot_multi_goal_planning.problems.configuration import config_dist
 
 
@@ -147,8 +148,12 @@ def main():
     )
     args = parser.parse_args()
 
-    np.random.seed(0)
-    random.seed(0)
+    folder_path = re.match(r'(.*?/out/[^/]+)', args.path_filename).group(1)
+    config = load_experiment_config(os.path.join(folder_path, 'config.json'))
+    num_run = re.search(r'/(\d+)(?=/)', args.path_filename).group(1)
+    seed = config["seed"] + int(num_run)
+    np.random.seed(seed)
+    random.seed(seed)
 
     path_data = load_path(args.path_filename)
     env = get_env_by_name(args.env_name)
