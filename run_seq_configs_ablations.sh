a6fb5fa86e67cea62d376d244eba74d34410817a
Tirza <theinle@ethz.ch>
2025-05-06 22:58:10 +0200

    add report config files

---
+++ run_seq_configs_ablations.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# List of config paths
+configs=(
+    "./configs/report/box_stacking_dep_batch_size.json"
+    "./configs/report/box_stacking_dep_mode_sampling.json"
+    "./configs/report/unassigned_cleanup_bacth_size.json"
+    "./configs/report/unassigned_cleanup_mode_sampling.json"
+)
+
+# Base command
+base_cmd="python3 examples/run_experiment.py"
+
+# Run each experiment
+for config in "${configs[@]}"; do
+    cmd="$base_cmd $config --parallel_execution --num_processes=6"
+    echo "Running: $cmd"
+    eval "$cmd"
+    echo "Finished: $cmd"
+done
+
+echo "All experiments completed."
