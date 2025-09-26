13e4b45a2fe57488a285bd7a9f697c8562b1eb4c
VH <v@vhartmann.com>
2025-09-26 13:47:39 +0200

    Add checks if mujoco/pinocchio can be imported

--- src/multi_robot_multi_goal_planning/problems/__init__.py
+++ src/multi_robot_multi_goal_planning/problems/__init__.py
@@ -1,14 +1,20 @@
 import os
 import sys
+import importlib.util
 
 from . import rai_envs
 from . import rai_single_goal_envs
 from . import rai_unordered_envs
 from . import rai_free_envs
 from . import abstract_env
-from . import pinocchio_env
 from . import rai_envs_constrained
-from . import mujoco_env
+
+
+if importlib.util.find_spec("pinocchio") is not None:
+    from . import pinocchio_env
+
+if importlib.util.find_spec("mujoco") is not None:
+    from . import mujoco_env
 
 from .registry import get_env_by_name, get_all_environments
 
