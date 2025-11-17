db40e51cd1e4490e4eb65b7a937917bcb93e13fb
VH <v@vhartmann.com>
2025-11-17 17:44:19 +0100

    Add priorized planner to run_all_envs

--- examples/run_all_envs.py
+++ examples/run_all_envs.py
@@ -9,7 +9,12 @@ from multi_robot_multi_goal_planning.problems import (
     get_env_by_name,
 )
 
-from multi_robot_multi_goal_planning.planners import BaseRRTConfig, BidirectionalRRTstar
+from multi_robot_multi_goal_planning.planners import (
+    BaseRRTConfig,
+    BidirectionalRRTstar,
+    PrioritizedPlannerConfig,
+    PrioritizedPlanner,
+)
 
 from multi_robot_multi_goal_planning.planners.termination_conditions import (
     RuntimeTerminationCondition,
@@ -17,24 +22,39 @@ from multi_robot_multi_goal_planning.planners.termination_conditions import (
 
 
 def make_planners(env, runtime):
-    def planner(env):
+    def birrt_planner(env):
         config = BaseRRTConfig()
         config.distance_metric = "max_euclidean"
         return BidirectionalRRTstar(env, config=config).plan(
             RuntimeTerminationCondition(runtime), optimize=True
         )
 
-    planners = [("birrt", planner)]
+    def prio_planner(env):
+        config = PrioritizedPlannerConfig()
+        return PrioritizedPlanner(env, config).plan(
+            RuntimeTerminationCondition(runtime), optimize=True
+        )
+
+    planners = [("birrt", birrt_planner), ("prio", prio_planner)]
     return planners
 
 
 def main():
     # all_envs = get_all_environments()
     representative_envs = [
-        "rai.simple",
-        "rai.one_agent_many_goals",
-        "rai.two_agents_many_goals_dep",
-        "rai.three_agent_many_goals",
+        # "rai.simple",
+        # "rai.one_agent_many_goals",
+        # "rai.two_agents_many_goals_dep",
+        # "rai.three_agent_many_goals",
+        # "rai.single_agent_mover",
+        "rai.piano_dep",
+        "rai.handover",
+        "rai.random_2d",
+        "rai.other_hallway",
+        "rai.three_agents",
+        "rai.triple_waypoints",
+        "rai.welding",
+        "rai.eggs",
     ]
 
     num_runs = 5
@@ -50,15 +70,15 @@ def main():
             experiment_folder = f"./out/{timestamp}_{experiment_name}_{env_name}/"
 
             if not os.path.isdir(experiment_folder):
-              os.makedirs(experiment_folder)
-    
+                os.makedirs(experiment_folder)
+
             config = {}
             config["seed"] = 0
             config["num_runs"] = num_runs
             config["environment"] = env_name
             config["max_planning_time"] = max_runtime
             config["planners"] = []
-            
+
             export_config(experiment_folder, config)
 
             env = get_env_by_name(env_name)
