8fdbadb4f06c8394dc5965242a7407003b9724f3
VH <v@vhartmann.com>
2025-11-22 15:54:22 +0100

    Add timings to run_all, add ait

--- examples/run_all_envs.py
+++ examples/run_all_envs.py
@@ -5,52 +5,52 @@ import datetime
 import os
 
 representative_envs = [
-    ("rai.simple", 2),
-    ("rai.one_agent_many_goals", 2),
-    ("rai.two_agents_many_goals_dep", 2),
-    ("rai.three_agent_many_goals", 2),
-    ("rai.single_agent_mover", 2),
-    ("rai.piano_dep", 2),
-    ("rai.2d_handover", 2),
-    ("rai.random_2d", 2),
-    ("rai.other_hallway", 2),
-    ("rai.three_agents", 2),
-    ("rai.triple_waypoints", 2),
-    ("rai.welding", 2),
-    ("rai.handover", 2),
-    ("rai.eggs", 2),
-    ("rai.bottles", 2),
-    ("rai.box_rearrangement", 2),
-    ("rai.box_reorientation", 2),
-    ("rai.box_reorientation_dep", 2),
-    ("rai.pyramid", 2),
-    ("rai.box_stacking", 2),
-    ("rai.box_stacking_dep", 2),
-    ("rai.mobile_wall_four", 2),
-    ("rai.mobile_wall_four_dep", 2),
-    ("rai.mobile_strut", 7),
-    ("rai.three_robot_truss", 7),
-    ("rai.spiral_tower", 0),
-    ("rai.spiral_tower_two", 0),
-    ("rai.cube_four", 2),
-    ("rai.unordered_box_reorientation", 0),
-    ("rai.unassigned_two_dim", 0),
-    ("rai.unassigned_cleanup", 0),
-    ("rai.unassigned_stacking", 0),
-    ("rai.unordered_bottles", 0),
+    ("rai.simple", 2, 100),
+    ("rai.one_agent_many_goals", 2, 100),
+    ("rai.two_agents_many_goals_dep", 2, 100),
+    ("rai.three_agent_many_goals", 2, 500),
+    ("rai.single_agent_mover", 2, 100),
+    ("rai.piano_dep", 2, 500),
+    ("rai.2d_handover", 2, 500),
+    ("rai.random_2d", 2, 500),
+    ("rai.other_hallway", 2, 500),
+    ("rai.three_agents", 2, 500),
+    ("rai.triple_waypoints", 2, 1000),
+    ("rai.welding", 2, 1000),
+    ("rai.handover", 2, 100),
+    ("rai.eggs", 2, 500),
+    ("rai.bottles", 2, 500),
+    ("rai.box_rearrangement", 2, 1000),
+    ("rai.box_reorientation", 2, 500),
+    ("rai.box_reorientation_dep", 2, 1000),
+    ("rai.pyramid", 2, 500),
+    ("rai.box_stacking", 2, 1000),
+    ("rai.box_stacking_dep", 2, 1000),
+    ("rai.mobile_wall_four", 2, 1000),
+    ("rai.mobile_wall_four_dep", 2, 1000),
+    ("rai.mobile_strut", 7, 1000),
+    ("rai.three_robot_truss", 7, 1000),
+    ("rai.spiral_tower", 0, 1000),
+    ("rai.spiral_tower_two", 0, 500),
+    ("rai.cube_four", 2, 1000),
+    ("rai.unordered_box_reorientation", 0, 1000),
+    ("rai.unassigned_two_dim", 0, 500),
+    ("rai.unassigned_cleanup", 0, 1000),
+    ("rai.unassigned_stacking", 0, 1000),
+    ("rai.unordered_bottles", 0, 500),
 ]
 
-num_parallel = 2
-num_runs = 1
-max_runtime = 500
-optimize = False
+num_parallel = 10
+num_runs = 25
+optimize = True
 
 planners = [
     {"name": "prio", "type": "prioritized", "options": {}},
     {"name": "birrt", "type": "birrtstar", "options": {}},
+    {"name": "ait", "type": "aitstar", "options": {}},
 ]
 
-for env_name, seed in representative_envs:
+for env_name, seed, runtime in representative_envs:
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     print(f"Running {env_name} with seed {seed}")
 
@@ -61,7 +61,7 @@ for env_name, seed in representative_envs:
             "environment": env_name,
             "per_agent_cost": "euclidean",
             "cost_reduction": "max",
-            "max_planning_time": max_runtime,
+            "max_planning_time": runtime,
             "num_runs": num_runs,
             "seed": seed,
             "optimize": optimize,
