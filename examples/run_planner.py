c8c6c56c3776d3e7a1514eb9d19a45078f563f27
21liam2000 <liam.schnyder@gmail.com>
2026-03-18 16:24:29 +0100

    Add profiling around planner

--- examples/run_planner.py
+++ examples/run_planner.py
@@ -40,6 +40,9 @@ from multi_robot_multi_goal_planning.planners import (
 
 from run_experiment import export_planner_data
 
+import cProfile
+import pstats
+
 import logging
 # logging.basicConfig(level=logging.INFO, format="%(name)s - %(levelname)s - %(message)s")
 logging.basicConfig(level=logging.INFO, format="%(message)s")
@@ -183,7 +186,16 @@ def main():
     np.random.seed(args.seed + args.run_id)
     random.seed(args.seed + args.run_id)
 
+    profiler_planner = cProfile.Profile()
+    profiler_planner.enable()
+
     path, info = planner.plan(ptc=termination_condition, optimize=args.optimize)
+    
+    profiler_planner.disable()
+    profiler_planner.dump_stats('profiler_planner.prof')
+    stats_planner = pstats.Stats(profiler_planner)
+    stats_planner.sort_stats('cumulative')
+    stats_planner.print_stats(40)
 
     if args.save:
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
